7e70cdec1a5e91d6641a924a24b6ddad69cee932
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2020-10-13 16:43:06 -0700

    Feat: add ability to complete a todo, and edit a todo

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,26 @@
       placeholder="Add a todo"
     />
     <ul class="todos">
-      <li v-for="todo in todos" :key="todo.id">
+      <li
+        v-for="todo in todos"
+        :key="todo.id"
+        @dblclick="editedToDo(todo)"
+        :class="{ completed: todo.completed, editing: todo == editedToDoId }"
+      >
         {{ todo.label }}
+        <input
+          v-model="todo.label"
+          v-show="editedToDoId == todo.id"
+          v-on:keydown.enter="updateToDo(todo)"
+          placeholder="Edit the todo"
+        />
+        <input
+          type="checkbox"
+          v-model="todo.completed"
+          @click="completedTodo(todo)"
+        />
+        <button @click="removeTodo(todo)">Delete</button>
       </li>
-      <button @click="removeTodo(todo)">Delete</button>
     </ul>
   </div>
 </template>
@@ -20,6 +36,8 @@ export default {
     return {
       todos: [],
       currentTodo: "",
+      editedToDoId: null,
+      completed: false,
     };
   },
   methods: {
@@ -35,8 +53,25 @@ export default {
       let index = this.todos.indexOf(todo);
       this.todos.splice(index, 1);
     },
+    completedTodo(todo) {
+      todo.completed = true;
+    },
+    editedToDo(todo) {
+      this.editedToDoId = todo.id;
+    },
+    updateToDo(todo) {
+      this.editedToDoId = null;
+    },
   },
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.completed {
+  text-decoration: line-through;
+}
+
+.editing {
+  display: none;
+}
+</style>
