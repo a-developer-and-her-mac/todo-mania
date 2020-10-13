ea8993cdac16d97733abde2e34ec3f9bea7e18ff
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2020-10-13 08:32:49 -0700

    Feat: add beginnings of a todo list, and a delete button

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,42 @@
-<template> </template>
+<template>
+  <div>
+    <input
+      v-model="currentTodo"
+      @keydown.enter="addTodo()"
+      placeholder="Add a todo"
+    />
+    <ul class="todos">
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.label }}
+      </li>
+      <button @click="removeTodo(todo)">Delete</button>
+    </ul>
+  </div>
+</template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      todos: [],
+      currentTodo: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.todos.length,
+        label: this.currentTodo,
+        completed: false,
+      });
+      this.currentTodo = "";
+    },
+    removeTodo(todo) {
+      let index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+  },
+};
+</script>
 
 <style lang="scss"></style>
