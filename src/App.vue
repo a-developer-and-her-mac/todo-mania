07d28a2fb39b63000027cc96cb429b285c72f482
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2020-10-14 12:17:15 -0700

    Style: update style of App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,51 @@
 <template>
-  <div>
-    <input
-      v-model="currentTodo"
-      @keydown.enter="addTodo()"
-      placeholder="Add a todo"
-    />
-    <ul class="todos">
-      <li
-        v-for="todo in todos"
-        :key="todo.id"
-        @dblclick="editedToDo(todo)"
-        :class="{ completed: todo.completed, editing: todo == editedToDoId }"
-      >
-        {{ todo.label }}
-        <input
-          v-model="todo.label"
-          v-show="editedToDoId == todo.id"
-          v-on:keydown.enter="updateToDo(todo)"
-          placeholder="Edit the todo"
-        />
-        <input
-          type="checkbox"
-          v-model="todo.completed"
-          @click="completedTodo(todo)"
-        />
-        <button @click="removeTodo(todo)">Delete</button>
-      </li>
-    </ul>
+  <div class="flex-container">
+    <md-card md-with-hover class="todo-card">
+      <md-ripple>
+        <md-card-header>
+          <div>Todo-Mania</div>
+        </md-card-header>
+        <md-card-content>
+          <md-field>
+            <md-input
+              v-model="currentTodo"
+              @keydown.enter="addTodo()"
+              placeholder="Add a todo"
+            />
+          </md-field>
+          <ul class="todos">
+            <li
+              v-for="todo in todos"
+              :key="todo.id"
+              @dblclick="editedToDo(todo)"
+              :class="{
+                completed: todo.completed,
+                editing: todo == editedToDoId,
+              }"
+            >
+              {{ todo.label }}
+              <input
+                v-model="todo.label"
+                v-show="editedToDoId == todo.id"
+                v-on:keydown.enter="updateToDo(todo)"
+                placeholder="Edit the todo"
+              />
+              <input
+                type="checkbox"
+                v-model="todo.completed"
+                @click="completedTodo(todo)"
+                class="checkbox"
+              />
+            </li>
+          </ul>
+          <md-card-actions>
+            <md-button @click="removeTodo(todo)" v-if="todoItem"
+              >Delete</md-button
+            >
+          </md-card-actions>
+        </md-card-content>
+      </md-ripple>
+    </md-card>
   </div>
 </template>
 
@@ -38,6 +57,7 @@ export default {
       currentTodo: "",
       editedToDoId: null,
       completed: false,
+      todoItem: false,
     };
   },
   methods: {
@@ -48,6 +68,7 @@ export default {
         completed: false,
       });
       this.currentTodo = "";
+      this.todoItem = true;
     },
     removeTodo(todo) {
       let index = this.todos.indexOf(todo);
@@ -67,11 +88,55 @@ export default {
 </script>
 
 <style lang="scss">
+ul {
+  list-style-type: none;
+}
+
+li {
+  margin-right: 20%;
+}
+
 .completed {
   text-decoration: line-through;
 }
 
 .editing {
   display: none;
+}
+
+.flex-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.md-card {
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  margin: 4px;
+  background-color: #fc9d9d;
+
+  @media (max-width: 600px) {
+    margin-top: 50%;
+  }
+
+  @media (min-width: 601px) {
+    margin-top: 20%;
+  }
+}
+
+.md-card-header {
+  text-align: center;
+  font-size: 20px;
+}
+
+.checkbox {
+  float: right;
+}
+
+.md-field input {
+  text-align: center;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {
+  MdField
+} from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
+import {
+  MdButton
+} from "vue-material/dist/components";
+import {
+  MdCard
+} from "vue-material/dist/components";
+import {
+  MdRipple
+} from "vue-material/dist/components";
+
+Vue.use(MdField);
+Vue.use(MdButton);
+Vue.use(MdCard);
+Vue.use(MdRipple);
 
 Vue.config.productionTip = false
 
